071425b1c411844b514d415105157d849bee07ea
Nicholas Osaka <nicholas.osaka@gmail.com>
2021-07-04 12:27:58 -0400

    Add Docker Compose file

--- api/src/logger.ts
+++ api/src/logger.ts
@@ -1,12 +1,21 @@
 import winston from 'winston';
 
-const loggingPath = 'logs/raas_api.log';
+const loggingPath = 'logs/api';
 
 export const logger = winston.createLogger({
     transports: [
         new winston.transports.File({
-            filename: loggingPath,
+            filename: `${loggingPath}_json.log`,
             level: 'info',
+        }),
+        new winston.transports.File({
+            filename: `${loggingPath}.log`,
+            format: winston.format.combine(
+                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                winston.format.splat(),
+                winston.format.printf(({ level, message, label, timestamp }) => `${timestamp} ${label || '-'} ${level}: ${message}`),
+            ),
+            level: 'info'
         }),
         new winston.transports.Console({
             level: 'debug',
