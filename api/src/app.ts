a34e6388e5252de47da02641c37d8d3792adebf9
Nicholas Osaka <nicholas.osaka@gmail.com>
2021-07-04 11:46:58 -0400

    Change container workdir

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,16 +3,16 @@ import morgan from 'morgan';
 import { logger } from "./logger";
 
 // API
-const api = express();
+const app = express();
 const port = 8080;
 
 const morganTokens = ':remote-addr :method :url :status :res[content-length] bytes in :response-time ms';
-api.use(morgan(morganTokens, { stream: { write: message => logger.info(message.trim()) } }))
+app.use(morgan(morganTokens, { stream: { write: message => logger.info(message.trim()) } }))
 
-api.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.status(200).json({ "msg": "RaaS" });
 })
 
-api.listen(port, () => {
+app.listen(port, () => {
     logger.info(`API listening on port ${port}`);
 })
